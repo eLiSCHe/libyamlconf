168d6b2b8db015306c1132c6b6d2fd6b13774eef
Tom Irgang <thomas@irgang.eu>
2025-03-03 08:35:14 +0100

    yaml parsing documentation

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ uv run ruff check --output-format=github .
 uv run coverage run -m pytest -v -s
 uv run coverage report -m
 uv run coverage html
+uv run darglint --verbosity 2 --docstring-style sphinx libyamlconf
