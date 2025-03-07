3fc7d633e9ab2623f596a085d2b977606033f132
Tom Irgang <thomas@irgang.eu>
2025-03-07 14:14:41 +0100

    support or file or URL

--- libyamlconf/__init__.py
+++ libyamlconf/__init__.py
@@ -117,4 +117,4 @@ Which would then also result in:
       - gdbserver
 """
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -3,6 +3,7 @@
 import logging
 
 from pathlib import Path
+from typing import Any
 
 import pytest
 
@@ -23,10 +24,26 @@ class Config(BaseModel):
     referenced: Path
 
 
+class ListWithFiles(BaseModel):
+    name: str
+    file: Path
+
+
+class ResolvePaths(BaseModel):
+    file: Path
+    url: Path | str
+    list_with_files: list[ListWithFiles]
+    file_list: list[Path | str]
+
+
+class InvalidFileList(BaseModel):
+    files: list[Path | str]
+
+
 class TestVerify:
     """Tests for config verification."""
 
-    def test_load_config(self):
+    def test_load_config(self) -> None:
         """Load and verify a valid configuration."""
         config_file = test_data / "config.yaml"
         files = [["referenced"]]
@@ -41,7 +58,7 @@ class TestVerify:
 
         verify_files_exist(config, files)
 
-    def test_additional_config(self, caplog):
+    def test_additional_config(self, caplog) -> None:
         """Unused config parameters shall be logged."""
         config_file = test_data / "not_used.yaml"
         files = [["referenced"]]
@@ -50,7 +67,7 @@ class TestVerify:
             load_and_verify(config_file, Config, relative_path_keys=files)
             assert "config file contains not used parameters" in caplog.text
 
-    def test_missing_config(self):
+    def test_missing_config(self) -> None:
         """Missing config parameter shall raise an ValidationError exception."""
         config_file = test_data / "missing.yaml"
         files = [["referenced"]]
@@ -58,7 +75,7 @@ class TestVerify:
         with pytest.raises(ValidationError):
             load_and_verify(config_file, Config, relative_path_keys=files)
 
-    def test_missing_referenced_file(self):
+    def test_missing_referenced_file(self) -> None:
         """Missing referenced file shall raise an InvalidConfiguration exception."""
         config_file = test_data / "invalid.yaml"
         files = [["referenced"]]
@@ -70,7 +87,7 @@ class TestVerify:
         with pytest.raises(InvalidConfiguration):
             verify_files_exist(config, files)
 
-    def test_invalid_file_spec(self, caplog):
+    def test_invalid_file_spec(self, caplog) -> None:
         """Not existing file key path shall raise an InvalidConfiguration exception."""
         config_file = test_data / "config.yaml"
         files = [["referenced"], ["wrong", "path"]]
@@ -81,3 +98,42 @@ class TestVerify:
 
         with pytest.raises(InvalidConfiguration):
             verify_files_exist(config, files)
+
+    def test_resolve_url(self) -> None:
+        """Test for resolving file paths."""
+        config_file = test_data.parent / "yaml" / "resolve_paths.yaml"
+        paths = [["file"], ["list_with_files", "file"], ["file_list"], ["url"]]
+
+        config: ResolvePaths = load_and_verify(
+            config_file,
+            ResolvePaths,
+            relative_path_keys=paths,
+        )
+
+        assert config.file == config_file.parent / "other_include.txt"
+        assert config.url == "http://www.google.de"
+        assert config.list_with_files[0].file == config_file.parent / "other" / "include.txt"
+        assert config.file_list[0] == config_file.parent / "other" / "include.txt"
+        assert config.file_list[1] == config_file.parent / "other_include.txt"
+        assert config.file_list[2] == "https://www.google.de"
+
+        verify_files_exist(model=config, relative_path_keys=paths)
+
+    def test_invalid_file_list(self) -> None:
+        """Test for resolving file paths."""
+        config_file = test_data / "invalid_file_list.yaml"
+        paths = [["files"]]
+
+        config: InvalidFileList = load_and_verify(
+            config_file,
+            InvalidFileList,
+            relative_path_keys=paths,
+        )
+
+        assert config.files[0] == test_data / "base.yaml"
+        assert config.files[1] == test_data / "config.yaml"
+        assert config.files[2] == "http://www.google.de"
+        assert config.files[3] == test_data / "invalid.txt"
+
+        with pytest.raises(InvalidConfiguration):
+            verify_files_exist(model=config, relative_path_keys=paths)
